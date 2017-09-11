806e68ede3d3fc268a1a3aee8d3c25c29db13c21
zgs225 <zgs225@gmail.com>
2017-09-11 13:21:33 +0800

    update

--- tests/TextBridgeTest.php
+++ tests/TextBridgeTest.php
@@ -1,16 +1,21 @@
 <?php
 
-class TextBridgeTest extends \PHPUnit\Framework\TestCase
+use PHPUnit\Framework\TestCase;
+use Prometheus\Bridges\TextFormatBridge;
+use Prometheus\Metrics\Counter;
+use Prometheus\Registry\BaseRegistry;
+
+class TextBridgeTest extends TestCase
 {
     public function testBridge()
     {
-        $registry = \Prometheus\Registry\BaseRegistry::defaultRegistry();
-        $counter  = new \Prometheus\Metrics\Counter("lord_v3", "test", "total", "测试计数器", []);
-        $counter2 = new \Prometheus\Metrics\Counter('lord_v3', 'test', 'total', '测试计数器', ['complex' => 4, 'status' => 1]);
+        $registry = BaseRegistry::defaultRegistry();
+        $counter  = new Counter("lord_v3", "test", "total", "测试计数器", []);
+        $counter2 = new Counter('lord_v3', 'test', 'total', '测试计数器', ['complex' => 4, 'status' => 1]);
         $registry->register($counter);
         $registry->register($counter2);
         $counter->inc();
-        $bridge   = new \Prometheus\Bridges\TextFormatBridge($registry);
+        $bridge   = new TextFormatBridge($registry);
         $text     = $bridge->bridge();
         $expected = <<< EOT
 # HELP lord_v3_test_total 测试计数器
