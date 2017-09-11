e8d9fc2c98f76ea1be76c6a1071455d0989969ea
zgs225 <zgs225@gmail.com>
2017-09-11 13:12:22 +0800

    添加测试

--- tests/TextBridgeTest.php
+++ tests/TextBridgeTest.php
@@ -6,7 +6,9 @@ class TextBridgeTest extends \PHPUnit\Framework\TestCase
     {
         $registry = \Prometheus\Registry\BaseRegistry::defaultRegistry();
         $counter  = new \Prometheus\Metrics\Counter("lord_v3", "test", "total", "测试计数器", []);
+        $counter2 = new \Prometheus\Metrics\Counter('lord_v3', 'test', 'total', '测试计数器', ['complex' => 4, 'status' => 1]);
         $registry->register($counter);
+        $registry->register($counter2);
         $counter->inc();
         $bridge   = new \Prometheus\Bridges\TextFormatBridge($registry);
         $text     = $bridge->bridge();
@@ -14,6 +16,9 @@ class TextBridgeTest extends \PHPUnit\Framework\TestCase
 # HELP lord_v3_test_total 测试计数器
 # TYPE lord_v3_test_total counter
 lord_v3_test_total 1
+# HELP lord_v3_test_total 测试计数器
+# TYPE lord_v3_test_total counter
+lord_v3_test_total{complex="4",status="1"} 0
 
 EOT;
         $this->assertEquals($expected, $text);
