f02907e2bc1ea77116f8e35926d9e3538b43655c
zgs225 <zgs225@gmail.com>
2017-09-12 09:28:45 +0800

    fix

---
+++ src/Providers/PrometheusServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Prometheus\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PrometheusServiceProvider extends ServiceProvider
+{
+    protected $defer = true;
+
+    public function register()
+    {
+    }
+
+    public function provides()
+    {
+    }
+}
---
+++ tests/app.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Illuminate\Container\Container;
+
+$container = new Container();
+Container::setInstance($container);
+
+$redis = new Illuminate\Redis\RedisManager('phpredis', [
+    'default' => [
+        'host'         => 'redis',
+        'port'         => 6379,
+        'password'     => null,
+        'database'     => 0,
+        'prefix'       => 'lord_v3',
+        'read_timeout' => 2
+    ]
+]);
+
+$container->instance('redis', $redis);
+
+return $container;
