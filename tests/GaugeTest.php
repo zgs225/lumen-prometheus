9352d0b5265f2f9619ac620532d7f7292ed763b6
zgs225 <zgs225@gmail.com>
2017-09-14 09:08:44 +0800

    add gauge test

---
+++ tests/GaugeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class GaugeTest extends TestCase
+{
+    public function testGauge()
+    {
+        $gauge = \Prometheus\Facades\Prometheus::gauge()
+            ->_namespace('lord_v3')
+            ->subsystem('test')
+            ->name('gauge')
+            ->help('测试仪表盘')
+            ->build();
+        $gauge->set(3.14);
+        $registry = new \Prometheus\Registry\BaseRegistry('test');
+        $registry->register($gauge);
+        $bridge   = new \Prometheus\Bridges\TextFormatBridge($registry);
+        $expected = <<< EOF
+# HELP lord_v3_test_gauge 测试仪表盘
+# TYPE lord_v3_test_gauge gauge
+lord_v3_test_gauge 3.14
+
+EOF;
+        $this->assertEquals($expected, $bridge->bridge());
+    }
+}
